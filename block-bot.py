045ee9bd5b21950a906af779989f7883cb34f3ed
Sajo8 <andrewsplin@protonmail.com>
2018-06-20 16:52:21 -0700

    its working now yay

--- block-bot.py
+++ block-bot.py
@@ -2,21 +2,37 @@ import discord
 import asyncio
 import time
 import json
+import logging
+import turtlecoin
 
+#discord stuff
+"""token = open('tokenfile').read()
+client = discord.Client()"""
 
-token = open('tokenfile').read()
-client = discord.Client()
+#logging/debugging stuff. exxtra noise in console which i dont like
+"""logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)"""
 
-
-@client.event
+#discord stuff
+"""@client.event
 async def on_ready():
-    print("connected")
+	print("connected")"""
 
-@client.event
+tc = turtlecoin.TurtleCoind(host='public.turtlenode.io', port=11898)
+bc = tc.getblockcount()
+bcd = bc['count']
+print("Blockcount:", bcd)
+
+#discord stuff
+"""@client.event
 async def on_message(message):
 	if message.content == "hi":
-		await client.send_message(message.channel, ":cookie:")
+		await client.send_message(message.channel, "hi")
 
-client.run(token)
+client.run(token)"""
 
 
