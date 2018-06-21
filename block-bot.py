51c43cfc34a431708df2381d0063fe9689ae129e
Sajo8 <andrewsplin@protonmail.com>
2018-06-20 22:22:08 -0700

    idk

--- block-bot.py
+++ block-bot.py
@@ -1,7 +1,5 @@
 import discord
 import asyncio
-import time
-import json
 import logging
 import turtlecoin
 
@@ -23,9 +21,37 @@ async def on_ready():
 	print("connected")"""
 
 tc = turtlecoin.TurtleCoind(host='public.turtlenode.io', port=11898)
-bc = tc.getblockcount()
-bcd = bc['count']
-print("Blockcount:", bcd)
+
+wd = turtlecoin.Walletd(password='test')
+
+tcgl = tc.getlastblockheader()['block_header']
+
+#height of the latest block, int
+height = tcgl['height']
+print("Height:", height)
+
+#hash of the latest block. str
+hash = tcgl['hash']
+print("Hash:", hash)
+
+#if latest block is orphan or not. bool(str) 
+orphan = tcgl['orphan_status']
+print("Orphan:", orphan)
+
+#reward of the latest block. int
+reward = tcgl['reward']
+print("Block reward:", reward, "TRTL")
+
+
+#transaction hashes. str
+txs = wd.get_transaction_hashes(addresses='', block_hash="715b65addef977a103e913aebd17071cd8440aefe2a6ad6fd1432b8bcd13efa5", block_count=1, payment_id='')
+
+print("No. of txs in the block:", len(txs))
+print("Transaction hashes:")
+
+for tx in txs:
+	print(tx)
+
 
 #discord stuff
 """@client.event
