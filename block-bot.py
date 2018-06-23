ce17b352708b3b4e438bc3f21dd81c5a0bc77960
Sajo8 <andrewsplin@protonmail.com>
2018-06-22 20:47:27 -0700

    jsut need to hex decode annd do discord.py

--- block-bot.py
+++ block-bot.py
@@ -3,6 +3,7 @@ import asyncio
 import logging
 import turtlecoin
 import json
+import requests
 
 #discord stuff
 """token = open('tokenfile').read()
@@ -20,6 +21,12 @@ logger.setLevel(logging.DEBUG)"""
 """@client.event
 async def on_ready():
 	print("connected")"""
+
+def prettyPrintDict(dict):
+	try:
+		print (json.dumps(dict, indent=4, sort_keys=True))
+	except Exception as e:
+		print('{}, Error: {}'.format('Failed to decode dict as json', e))
 
 tc = turtlecoin.TurtleCoind(host='public.turtlenode.io', port=11898)
 
@@ -39,19 +46,74 @@ print("Orphan:", orphan)
 
 #reward of the latest block. int
 reward = tcgl['reward']
-print("Block reward:", reward, "TRTL")
+print("Block reward:", reward / 100, "TRTL")
+
+time = tcgl['timestamp']
+prevhash = tcgl['prev_hash']
+time2 = tc.getlastblockheaderbyhash(prevhash)['block_header']['timestamp']
+
+timed = time - time2
+
+if timed <= 10:
+	print("block made way too fast")
+elif timed >= 90:
+	print("block took way too long")
+else:
+	print("perfect time yeah")
+
+	# also ping rockk
+
 
 bsize = tc.getblock(hash)
-print("Size of block:", bsize['block']['blockSize'])
+bsizes = bsize['block']['blockSize']
+print("Size of block:", bsizes, "a.u.")
 
 #transaction hashes. str
 txs = tc.getblock(hash)
 ntxs = len(txs['block']['transactions'])
 
 print("No. of txs in the block:", ntxs) 
-print("wee woo")
-print("Transaction hashes:")
-print(txs)
+print("Tx hashes in the block:")
+
+"""for transaction in txs['block']['transactions']:
+	print(transaction['hash'])"""
+
+hashes = [x['hash'] for x in txs['block']['transactions']]
+prettyPrintDict(hashes)
+
+print("Size of each tx:")
+
+hahsizes = [z['size'] for z in txs['block']['transactions']]
+prettyPrintDict(hahsizes)
+
+print("Total size of the transactions:")
+txsize = txs['block']
+txsizes = txsize['transactionsCumulativeSize']
+print(txsizes, "a.u.")
+
+
+for hash in hashes:
+	teta = tc.gettransaction(hash)['tx']['extra']
+	print("tx_extra hash:")
+	prettyPrintDict(teta)
+	print("Decoded version of tx_extra:")	
+	data = '{"jsonrpc":"2.0","id":"test","method":"f_transaction_json","params":{"hash":teta}}'
+	response = requests.post('https://blocks.turtle.link/daemon/json_rpc', data=data)
+	print(response)
+
+	# idk how to hex decode mman 
+	# hex decode the tx extras some how
+	
+
+txes =  bsizes-txsizes
+print("\nSize of tx_extra:", txes, "a.u.")
+
+
+txp = txsizes/bsizes * 100
+txep = txes/bsizes * 100
+
+print("Percentage of transactions in the block:", txp, "%")
+print("Percentage of tx_extra in the block:", txep, "%")
 
 
 
