9f790f0d3d41c624a246ed0c88030acd952dabc3
Sajo8 <andrewsplin@protonmail.com>
2018-06-24 12:34:14 -0700

    .

--- bbot.py
+++ bbot.py
@@ -19,114 +19,147 @@ tc = turtlecoin.TurtleCoind(host='public.turtlenode.io', port=11898)
 
 tcgl = tc.getlastblockheader()['block_header']
 
-"""def hi():
-	last_height = 0
-	height = 0
-
-	while True:
-		height = tcgl['height']
-		if height != last_height:
-			last_height = height
-			info = getDaemonInfo()
-			msg = prettyPrintInfo(info)
-			client.send_message(channel, msg)
-		time.sleep(5)
-
-async def getDaemonInfo():
-	hash = tcgl['hash']
-	return {'hash': hash}
-
-async def prettyPrintInfo(blockInfo):
-	msg = ""
-	msg += f"Hash: {blockInfo['hash']}"
-	return msg"""
-
-
-height = tcgl['height']
-hash = tcgl['hash']
-orphan = tcgl['orphan_status']
-
-reward = tcgl['reward']
-breward = reward / 100
-
-timex = tcgl['timestamp']
-prevhash = tcgl['prev_hash']
-glb = tc.getblock(prevhash)
-time2 = glb['block']['timestamp']
-timed = timex - time2
-rock = "235707623985512451"
-blocktime = "Time took to make: "
-if timed <= 10:
-	blocktime += "Block was too fast, {timed} seconds <@{rock}>".format(timed=timed, rock=rock)
-elif timed >= 90:
-	blocktime += "Took too long, {timed} seconds. <@{rock}>".format(timed=time, rock=rock)
-else:
-	blocktime += "Took {timed} seconds to make, pretty nice".format(timed=timed)
-
-bsize = tc.getblock(hash)
-bsizes = bsize['block']['blockSize']
-
-txs = tc.getblock(hash)
-ntxs = len(txs['block']['transactions'])
-
-hashes = [x['hash'] for x in txs['block']['transactions']]
-
-hahsizes = [z['size'] for z in txs['block']['transactions']]
-
-txsize = txs['block']
-txsizes = txsize['transactionsCumulativeSize']
-
-for hash in hashes:
-	#tx extra hash
-	teta = tc.gettransaction(hash)['tx']['extra']
-#	Decoded version of tx_extra:
-	try:
-		deteta = bytes.fromhex(teta[66:]).decode('utf-8')
-	except UnicodeDecodeError:
-		deteta = "unable to decode, probably nothing in there"
-
-	#hex decode tx extra somehow
-
-txes =  bsizes-txsizes
-
-txp = txsizes/bsizes * 100
-
-txep = txes/bsizes * 100
-
 def getstats():
+
+	height = tcgl['height']
+	hash = tcgl['hash']
+	orphan = tcgl['orphan_status']
+
+	reward = tcgl['reward']
+	breward = reward / 100
+
+	timex = tcgl['timestamp']
+	prevhash = tcgl['prev_hash']
+	glb = tc.getblock(prevhash)
+	time2 = glb['block']['timestamp']
+	timed = timex - time2
+	rock = "235707623985512451"
+	blocktime = ""
+	if timed <= 10:
+		blocktime += "Block was too fast, {timed} seconds <@{rock}>".format(timed=timed, rock=rock)
+	elif timed >= 90:
+		blocktime += "Took too long, {timed} seconds. <@{rock}>".format(timed=time, rock=rock)
+	else:
+		blocktime += "Took {timed} seconds to make, pretty nice".format(timed=timed)
+
+	bsize = tc.getblock(hash)
+	bsizes = bsize['block']['blockSize']
+
+	txs = tc.getblock(hash)
+	ntxs = len(txs['block']['transactions'])
+
+	hashes = [x['hash'] for x in txs['block']['transactions']]
+
+	hahsizes = [z['size'] for z in txs['block']['transactions']]
+
+	txsize = txs['block']
+	txsizes = txsize['transactionsCumulativeSize']
+
+	for hash in hashes:
+		#tx extra hash
+		teta = tc.gettransaction(hash)['tx']['extra']
+	#	Decoded version of tx_extra:
+		try:
+			deteta = bytes.fromhex(teta[66:]).decode('utf-8')
+		except UnicodeDecodeError:
+			deteta = "unable to decode, probably nothing in there"
+
+	txes =  bsizes-txsizes
+
+	txp = txsizes/bsizes * 100
+
+	txep = txes/bsizes * 100
+
+	return {'height': height, 'hash': hash, 'orphan': orphan, 'reward': breward, 'bsizes': bsizes, 'blocktime': blocktime, 'ntxs': ntxs, 'hashes': hashes, 'hahsizes': hahsizes, 'txsizes': txsizes, 'teta': teta, 'deteta': deteta, 'txes': txes, 'txp': txp, 'txep': txep}
+
 	#height of the latest block, int
-	height
+	#height
 	#hash of the latest block. str
-	hash 
+	#hash 
 	#if latest block is orphan or not. bool(str) 
-	orphan 
+	#orphan 
 	#reward of the latest block. int
-	breward
+	#breward
 	#if time block took to make is acceptable
-	blocktime
+	#blocktime
 	# size of block
-	bsizes
+	#bsizes
 	#transaction hashes. str
-	ntxs
+	#ntxs
 	#hash of each tx in the block
-	hashes
+	#hashes
 	#size of each tx
-	hahsizes
+	#hahsizes
 	#print("Total size of the transactions:")
-	txsizes
+	#txsizes
 	#print out the tx_Extra hash and its decoded version
-	teta
-	deteta
+	#teta
+	#deteta
 	#size of tx extra
-	txes
+	#txes
 	#percentage of txs in block
-	txp
+	#txp
 	#percentage of tx_extra in block
-	txep
+	#txep
 
-getstats()
+def prettyPrintStats(blockstats):
+	msg = "BLOCK STATS:\n"
+	msg += "Height: {} \n".format(blockstats['height'])
+	msg += "Hash: {} \n".format(blockstats['hash'])
+	msg += "Orphan: {} \n".format(blockstats['orphan'])
+	msg += "Reward {} \n".format(blockstats['reward'])
+	msg += "Size: {} \n".format(blockstats['bsizes'])
+	msg += "Time took to make: {}".format(blockstats['blocktime'])
 
-printstats = """We just found a block!
+	msg += " \nNo. of txs in the block: {} \n".format(blockstats['ntxs'])
+	msg += "Tx hashes in the block: {} \n".format(blockstats['hashes'])
+	msg += "Size of each tx: {} \n".format(blockstats['hahsizes'])
+	msg += "Size of all the txs: {} \n".format(blockstats['txsizes'])
+
+	msg += "tx_extra hash: {} \n".format(blockstats['teta'])
+	msg += "Decoded version of tx_extra: {} \n".format(blockstats['deteta'])
+	msg += "Size of tx_extra: {} \n".format(blockstats['txes'])
+
+	msg += "Percentage of txs in the block: {} % \n".format(blockstats['txp'])
+	msg += "Percentage of tx_extra in the block: {} % \n".format(blockstats['txep'])
+
+	msg += "------------------"
+
+	return msg
+
+@client.event
+async def on_ready():
+	print("connected")
+	height = tcgl['height']
+	while True:
+		nheight = tc.getblockcount()['count']
+		print(nheight)
+		print(height)
+		if height != nheight:
+			await client.send_message(discord.Object(id='459931714471460864'), prettyPrintStats(nheight))
+			height = nheight
+			print(height)
+			print(nheight)
+		time.sleep(0.4)
+	
+
+	"""await client.send_message(discord.Object(id='459931714471460864'), printstats)
+	while True:
+		nheight = tc.getblockcount()['count']
+		if nheight != height:
+			print(nheight)
+			print(height)
+			print(printstats)
+			await client.send_message(discord.Object(id='459931714471460864'), printstats)
+			time.sleep(0.4)
+		else:
+			print("nope")
+			print(neight)
+			print(height)
+			time.sleep(0.4)"""
+
+
+"""printstats = We just found a block!
 
 Height: {height}
 Hash: {hash}
@@ -148,29 +181,7 @@ Percentage of txs in the block: {txp} %
 Percentage of tx_extra in the block: {txep} %
 
 -----------------------------------------------
-""".format(height=height, hash=hash, orphan=orphan, reward=breward, bsizes=bsizes, ntxs=ntxs, hashes=hashes, hashsizes=hahsizes, blocktime=blocktime, teta=teta, deteta=deteta, txes=txes, txp=txp, txep=txep, txsizes=txsizes)
-
-while True:
-	getstats()
-
-
-@client.event
-async def on_ready():
-	print("connected")
-	await client.send_message(discord.Object(id='459931714471460864'), printstats)
-	while True:
-		nheight = tc.getblockcount()['count']
-		if nheight != height:
-			print(nheight)
-			print(height)
-			print(printstats)
-			await client.send_message(discord.Object(id='459931714471460864'), printstats)
-			time.sleep(0.4)
-		else:
-			print("nope")
-			print(neight)
-			print(height)
-			time.sleep(0.4)
+.format(height=height, hash=hash, orphan=orphan, reward=breward, bsizes=bsizes, ntxs=ntxs, hashes=hashes, hashsizes=hahsizes, blocktime=blocktime, teta=teta, deteta=deteta, txes=txes, txp=txp, txep=txep, txsizes=txsizes)"""
 
 
 
