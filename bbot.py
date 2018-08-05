f214f9723fa3a9ac24821af32485c66479e89414
Sajo8 <andrewsplin@protonmail.com>
2018-08-05 10:29:22 -0700

    .

--- bbot.py
+++ bbot.py
@@ -2,37 +2,111 @@ import discord
 import asyncio
 import turtlecoin
 import json
+import sys
 import time
 import random
 
-#discord stuff
+# discord stuff
+client = None
+tc = None
+tclbh = None
+
+token = None
+"""
 token = open('tokenfile').read()
-client = discord.Client()
 
-tc = turtlecoin.TurtleCoind(host='public.turtlenode.io', port=11898)
+if token:
+	if token == 'YOUR-TOKEN-HERE':
+		token = None
+	else:
+		client = discord.Client()
+"""
+client = None
 
-tclbh = tc.get_last_block_header()['result']
+
+def connect_to_turtlecoind():
+	global tc
+	global tclbh
+	tc = turtlecoin.TurtleCoind(host='public.turtlenode.io', port=11898)
+	try:
+		tclbh = tc.get_last_block_header()['result']
+	except (JSONDecodeError, ConnectionError):
+		while tc.getblockcount()['json_rpc'] != "2.0":
+			asyncio.sleep(5)
+			tclbh = tc.get_last_block_header()['result']
+
+
+def decode_tx_extra(tx_extra_hex):
+	"""Returns string data decoded from the Transaction Extra Field
+	See https://cryptonote.org/cns/cns005.txt for proper formatting of tx_extra_hex
+	"""
+	curr_index = 0
+	custom_data_arr = []
+	payment_id = None
+	tx_extra_decoded = ''
+
+	if not isinstance(tx_extra_hex, str):
+		return tx_extra_decoded
+
+	while(curr_index < len(tx_extra_hex)):
+		# Each subfield must begin with a tag 00, 01, or 02
+		if tx_extra_hex.startswith('00', curr_index):  # padding
+			# All zeroes from here. No need to report this. We're done!
+			curr_index = len(tx_extra_hex)
+		elif tx_extra_hex.startswith('01', curr_index):  # payment ID
+			curr_index += 2
+			payment_id = tx_extra_hex[curr_index:curr_index + 64]
+			curr_index += 64
+		elif tx_extra_hex.startswith('02', curr_index):  # extra nonce (custom data)
+			# next byte will specify size
+			curr_index += 2
+			subfield_size = 2 * int(tx_extra_hex[curr_index:curr_index + 2], 16)
+			curr_index += 2
+			data = tx_extra_hex[curr_index:curr_index + subfield_size]
+			curr_index += subfield_size
+			custom_data_arr.append(data)
+			curr_index += subfield_size
+		else:
+			tx_extra_decoded += f"Hm, something went wrong. I got an invalid subfield tag of {tx_extra_hex[curr_index:curr_index + 2]}\n"
+			curr_index += 2
+
+	if payment_id:
+		tx_extra_decoded += f"\nPayment ID: {payment_id}"
+
+	for hash in custom_data_arr:
+		utf8_str = None
+		tx_extra_decoded += f"\nCustom Data Hex: {hash}"
+		# try decoding as utf-8 data as well
+		try:
+			utf8_str = bytearray.fromhex(hash).decode()
+		except ValueError as err:
+			pass
+
+		if utf8_str:
+			tx_extra_decoded += f"\nCustom Data UTF-8: {utf8_str}"
+
+	return tx_extra_decoded
+
 
 def getstats(height):
-
 	tcgl = tc.get_last_block_header()['result']['block_header']
 
-	#height of the latest block
+	# height of the latest block
 	height = tcgl['height']
-	#hash of latest block
+	# hash of latest block
 	hash = tcgl['hash']
 	# whether atest block is orphan or not
 	orphan = tcgl['orphan_status']
 
-	#reward of the latest block
+	# reward of the latest block
 	reward = tcgl['reward']
 	breward = reward / 100
 
-	#wheter the time the block took to make is acceptable or not
+	# whether the time the block took to make is acceptable or not
 	timex = tcgl['timestamp']
 	prevhash = tcgl['prev_hash']
-	glb = tc.get_block(prevhash)['result']
-	time2 = glb['block']['timestamp']
+	glb = tc.get_block(prevhash)
+	time2 = glb['result']['block']['timestamp']
 	timed = timex - time2
 	rock = "388916188715155467"
 	pingrock = "<@" + rock + ">"
@@ -47,84 +121,90 @@ def getstats(height):
 		blocktime += f"Took {timed} seconds to make, pretty nice"
 		pingrock = ""
 
-	#size of the block
-	bsize = tc.get_block(hash)['result']
-	bsizes = bsize['block']['blockSize']
+	# size of the block
+	txs = tc.get_block(hash) #changed 2 vars to one var, keeping name. 
+	bsizes = txs['result']['block']['blockSize']
 
 	# number of transaction hashes in the block
-	txs = tc.get_block(hash)['result']
-	ntxs = len(txs['block']['transactions'])
+	ntxs = len(txs['result']['block']['transactions'])
 
-	#each tx hash in the block
-	hashes = [x['hash'] for x in txs['block']['transactions']]
+	# each tx hash in the block
+	hashes = [x['hash'] for x in txs['result']['block']['transactions']]
 
 	# size of each tx
-	hahsizes = [z['size'] for z in txs['block']['transactions']]
+	hahsizes = [z['size'] for z in txs['result']['block']['transactions']]
 
-	#size of all the txs
-	txsize = txs['block']
+	# size of all the txs
+	txsize = txs['result']['block']
 	txsizes = txsize['transactionsCumulativeSize']
 
-
 	for hash in hashes:
-		#tx extra hash
+		# tx extra hash
 		teta = tc.get_transaction(hash)['result']['tx']['extra']
-		#Decoded version of tx_extra:
-		try:
-			deteta = bytes.fromhex(teta).decode('utf-8')
-		except UnicodeDecodeError:
-			deteta = "unable to decode, probably nothing in there"
+		# Decoded version of tx_extra:
+		deteta = decode_tx_extra(teta)
 
-	#size of tx extra		
-	txes =  bsizes-txsizes
+	# size of tx extra
+	txes = bsizes - txsizes
 
 	# % of txs in the block
-	txp = txsizes/bsizes * 100
+	txp = txsizes / bsizes * 100
 
 	# % of tx_extra in the block
-	txep = txes/bsizes * 100
+	txep = txes / bsizes * 100
 
 	return {'height': height, 'hash': hash, 'orphan': orphan, 'reward': breward, 'bsizes': bsizes, 'blocktime': blocktime, 'ntxs': ntxs, 'hashes': hashes, 'hahsizes': hahsizes, 'txsizes': txsizes, 'teta': teta, 'deteta': deteta, 'txes': txes, 'txp': txp, 'txep': txep, 'pingrock': pingrock}
 
 
 def prettyPrintStats(blockstats):
 	msg = "```WE FOUND A NEW BLOCK!\n"
-	msg += f"\nHeight: {blockstats['height']} \n"
-	msg += f"Hash: {blockstats['hash']} \n"
-	msg += f"Orphan: {blockstats['orphan']} \n"
-	msg += f"Reward: {blockstats['reward']} \n"
-	msg += f"Size: {blockstats['bsizes']} \n"
-	msg += f"Time took to make: {blockstats['blocktime']} \n"
+	msg += "\nHeight: {} \n".format(blockstats['height'])
+	msg += "Hash: {} \n".format(blockstats['hash'])
+	msg += "Orphan: {} \n".format(blockstats['orphan'])
+	msg += "Reward: {} \n".format(blockstats['reward'])
+	msg += "Size: {} \n".format(blockstats['bsizes'])
+	msg += "Time took to make: {} \n".format(blockstats['blocktime'])
 
-	msg += f" \nNo. of txs in the block: {blockstats['ntxs']} \n"
-	msg += f"Tx hashes in the block: {blockstats['hashes']} \n"
-	msg += f"Size of each tx: {blockstats['hahsizes']} \n"
-	msg += f"Size of all the txs: {blockstats['txsizes']} \n \n"
+	msg += " \nNo. of txs in the block: {} \n".format(blockstats['ntxs'])
+	msg += "Tx hashes in the block: {} \n".format(blockstats['hashes'])
+	msg += "Size of each tx: {} \n".format(blockstats['hahsizes'])
+	msg += "Size of all the txs: {} \n \n".format(blockstats['txsizes'])
 
-	msg += f"tx_extra hash: {blockstats['teta']} \n"
-	msg += f"Decoded version of tx_extra: {blockstats['deteta']} \n"
-	msg += f"Size of tx_extra: {blockstats['txes']} \n \n"
+	msg += "tx_extra hash: {} \n".format(blockstats['teta'])
+	msg += "Decoded version of tx_extra: {} \n".format(blockstats['deteta'])
+	msg += "Size of tx_extra: {} \n \n".format(blockstats['txes'])
 
-	msg += f"Percentage of txs in the block: {blockstats['txp']} % \n"
-	msg += f"Percentage of tx_extra in the block: {blockstats['txep']} % ```"
+	msg += "Percentage of txs in the block: {} % \n".format(blockstats['txp'])
+	msg += "Percentage of tx_extra in the block: {} % ```".format(blockstats['txep'])
 
-	#msg += blockstats['pingrock']
-
-	return msg
+	# msg += blockstats['pingrock']
 	print(msg)
 
+	return msg
 
-@client.event
+
+class client_check(object):
+	def __init__(self, client):
+		self.client = client
+
+	def __call__(self, func):
+		if not self.client:
+			print("Cannot connect to Discord without a valid token. Bot will attempt to print to console.")
+			# Return the function unchanged, not decorated with Discord API.
+			return func
+		return self.client.event(func)
+
+
+@client_check(client)
 async def on_ready():
 	print("connected")
 	height = tclbh['block_header']['height']
 	while True:
-		#prettyPrintStats(getstats(nheight))	
 		nheight = tc.get_block_count()['result']['count']
 		if height != nheight:
 			prettyPrintStats(getstats(nheight))
-			
-			await client.send_message(discord.Object(id='459931714471460864'), prettyPrintStats(getstats(nheight)))
+			if client:
+				await client.send_message(discord.Object(id='459931714471460864'), prettyPrintStats(getstats(nheight)))
 			print("val changed")
 			print(nheight)
 			print(height)
@@ -133,4 +213,21 @@ async def on_ready():
 		await asyncio.sleep(0.5)
 
 
-client.run(token)
+def start_local_event_loop():
+	print("*** Starting local event loop with TurtleCoind ***")
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(on_ready())
+	loop.close()
+
+
+if __name__ == '__main__':
+	connect_to_turtlecoind()
+
+	if client:
+		client.run(token)
+	else:
+		try:
+			start_local_event_loop()
+		except KeyboardInterrupt as err:
+			print("\nShutdown requested. Exiting...")
+			sys.exit(0)
